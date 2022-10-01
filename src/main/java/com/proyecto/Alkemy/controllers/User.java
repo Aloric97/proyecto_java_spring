eda4a13a0b7b5f7f84db2c1ad66dca84e85879a3
Leandro Tombetta <57770761+Aloric97@users.noreply.github.com>
2022-09-30 23:09:36 -0300

    creacion tabla users

---
+++ src/main/java/com/proyecto/Alkemy/controllers/User.java
@@ -0,0 +1,59 @@
+package com.proyecto.Alkemy.controllers;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    public User() {}
+
+    public User (String username, String email, String password){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+
+    public Long getId_persona() {
+        return id_persona;
+    }
+
+    public void setId_persona(Long id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
